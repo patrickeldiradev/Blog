8c0f81a20af711f34c4fdfc7dda6ac922eafb31e
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Use cache for list and show posts.

--- app/Http/Controllers/Site/PostController.php
+++ app/Http/Controllers/Site/PostController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Site\Posts\IndexPostsRequest;
 use App\Repository\PostRepository;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Cache;
+
+use Illuminate\Support\Facades\Request;
 
 use function view;
 
@@ -27,7 +31,9 @@ class PostController extends Controller
      */
     public function index(IndexPostsRequest $request)
     {
-        $posts = $this->postRepository->paginatedPosts();
+        $posts = Cache::remember($this->hashUrl(), 600, function () {
+            return $this->postRepository->paginatedPosts();
+        });
 
         return view('site.posts.index', compact('posts'));
     }
@@ -39,8 +45,25 @@ class PostController extends Controller
      */
     public function show(int $postId)
     {
-        $post = $this->postRepository->getPostById($postId);
+        $post = Cache::remember($this->hashUrl(), 600, function () use($postId) {
+            return $this->postRepository->getPostById($postId);
+        });
 
         return view('site.posts.show', compact('post'));
+    }
+
+    /**
+     * @param FormRequest $request
+     * @return string
+     */
+    protected function hashUrl(): string
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+        ksort($queryParams);
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{$url}?{$queryString}";
+
+        return sha1($fullUrl);
     }
 }
